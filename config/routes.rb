d1a8062ae27e91cade772a699cfd5346a58471dd
nemdull <dlanx@outlook.jp>
2021-11-17 02:29:05 +0000

    Fav&Comment

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#top'
   get "home/about"
-  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
+    resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :update, :index]
 end
